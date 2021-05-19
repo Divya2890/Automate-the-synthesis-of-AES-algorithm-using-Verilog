63ad7208aff6f21759d45ef6f5a38dc68bf73201
Divya2890 <51473222+Divya2890@users.noreply.github.com>
2021-05-19 13:46:54 +0530

    Encryption Code

---
+++ Encryption/main_tb_1.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   19:27:29 03/14/2021
+// Design Name:   main
+// Module Name:   M:/VLSI/enc_decryp/main_tb.v
+// Project Name:  enc_decryp
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: main
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module main_tb;
+
+	// Inputs
+	reg [0:127] data;
+	reg [0:127] key;
+
+	// Outputs
+	wire [0:127] en_key;
+
+	// Instantiate the Unit Under Test (UUT)
+	main uut (
+		.data(data), 
+		.key(key), 
+		.en_key(en_key)
+	);
+
+	initial begin
+		// Initialize Inputs
+		//$monitor($time,"output is %h",en_key);
+		//data ="This is the aes";
+		//key ="1234" ;
+		#10;
+		if($value$plusargs ("data=%s",data))
+			//$display("data is %s",data);
+		if($value$plusargs ("key=%s",key))
+			//$display("key is %s",key);
+		$monitor("%h",en_key);
+
+		
+		// Add stimulus here
+	end
+      
+endmodule
+
---
+++ Encryption/shift_rows.v
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    18:50:37 03/14/2021 
+// Design Name: 
+// Module Name:    shift_rows 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module shift_rows(b_shift,a_shift);
+input [0:127] b_shift;
+output [0:127] a_shift;
+
+assign a_shift[0:7]   = b_shift[0:7]   ;//0 
+assign a_shift[8:15] = b_shift[40:47];//1
+assign a_shift[16:23] = b_shift[80:87];//2
+assign a_shift[24:31] = b_shift[120:127];//3
+
+assign a_shift[32:39] = b_shift[32:39];//4
+assign a_shift[40:47] = b_shift[72:79];//5
+assign a_shift[48:55] = b_shift[112:119];//6
+assign a_shift[56:63] = b_shift[24:31];//7
+
+assign a_shift[64:71] = b_shift[64:71];//8
+assign a_shift[72:79] = b_shift[104:111];//9
+assign a_shift[80:87] = b_shift[16:23];//10
+assign a_shift[88:95] = b_shift[56:63];//11
+
+assign a_shift[96:103] = b_shift[96:103];//12
+assign a_shift[104:111] = b_shift[8:15];//13s
+assign a_shift[112:119] = b_shift[48:55];//14
+assign a_shift[120:127] = b_shift[88:95];//15
+
+endmodule
+
+
+
+
